40d3e108cafb21ec873739043e55bcfb1dad7342
shoaib soomro <shoaibkhanu39@gmail.com>
2024-05-16 11:16:36 +0500

    js-2nd class

---
+++ index.js
@@ -0,0 +1,99 @@
+// const  num = 77;
+
+// num  = 776
+// num = 33;
+// console.log(num);
+
+
+// let num = "44";
+// let num2 = 66;
+
+// alert( num + num2);
+// console.log(num + num2);
+
+// let firstName = "Muhammad";
+//  let lastname = "shoaib";
+
+// console.log(firstName + " " + lastname);
+
+
+// number + number addition 
+// num + string concatenation 
+// String  + number concatenation
+
+// let email = "asd@gamil.com"
+// let name = "abdullah"
+// let account = 7893783267;
+// let etc 
+// console.table([email , name , account, etc])
+
+// math operators  =  +  -  *  / divided % modules
+
+// let num = 66;
+// let num2 = 1;
+// console.log(num + num2);
+
+
+// let num = "66";
+// let num2 = 1;
+// console.log(num + num2);
+
+// let data = 100;
+// let data2 = 5;
+// console.log(data * data2);
+
+// let data = 100;
+// let data2 = 5;
+// console.log(data / data2);
+
+// let data = 10; 
+// let data2 = 3;
+// let add = data % data2;
+// console.log(add);
+
+// let data = 11; 
+// let data2 = 3;
+// let add = data % data2;
+// console.log(add);
+
+
+// let name = prompt("what is your name ............ ");
+
+// 
+// document.write(name)
+
+// let name = prompt("what is your number ............ ");
+// console.log(name);
+
+// let rendomNumbwer1 = Math.random();
+// console.log(rendomNumbwer1)
+
+// let rendomNumbwer = Math.random();
+// rendomNumbwer = rendomNumbwer * 10; 
+// let rounded = Math.round(rendomNumbwer)
+// console.log(rounded)
+// BODMAS  =  BARKET  OPEN DIVIDED MULPLY ADD SUBTRA 
+// let num = 4 + 6 * (5 + 5) ;
+// console.log(num);
+
+
+// let x = 33;
+// console.log(x);
+// console.log(typeof String(x));
+
+// let name = null;
+// console.log(name);
+
+/////Boolean
+//  let x = 33;
+//  let y = false;
+
+// console.log(typeof y);
+
+// console.log(x + y)
+
+
+// let x = 33; 
+// let y = true ;
+// console.log(x + 12 + y);
+
