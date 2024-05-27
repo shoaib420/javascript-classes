963364bec7fb2f58a68839105cebe3db88f791ae
shoaib soomro <shoaibkhanu39@gmail.com>
2024-05-27 11:09:00 +0500

    js-array-6class

---
+++ js-6-class/index.js
@@ -0,0 +1,17 @@
+///////////////////array ;
+ let arr = ["shaoib ", "abdullah", "wasif ", "waseem"];
+//  arr[0] = "wasif-"// not recommended
+//   arr.push("naveed") // array value add end
+  arr.unshift("ali") // array first value added
+//   arr.pop()  // array last value remove 
+//   arr.shift() // array first value remove 
+// arr.length
+// console.log(arr);
+//  console.log(arr.length);
+arr.splice(2, 0, "danish", 'noman') // added and remove or replace
+let b = arr.slice(0,4)
+console.log(arr);
+console.log(b);
+// let a =  [1, 3, 4,5 , 7, 8, 8., 9, 0,];
+// console.log(a);
+// console.log(a.length);
