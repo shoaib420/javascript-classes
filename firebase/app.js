1e4d80c6c39587d519ac958fa1ac0e7e0663012f
shoaib soomro <shoaibkhanu39@gmail.com>
2024-07-10 11:43:52 +0500

    firebase-login-signup

--- firebase/app.js
+++ firebase/app.js
@@ -15,3 +15,20 @@ const signup = async (e)=>{
  }
 
 }
+
+const login = async (e)=>{
+   e.preventDefault()
+   const email = document.getElementById("login_email").value;
+   const password = document.getElementById("login_password").value;
+   // console.log(email, password);
+
+try {
+   const result = await firebase.auth().signInWithEmailAndPassword(email, password);
+   console.log(result);
+   alert(`user is successfully login ${result.user.email}`)
+} catch (error) {
+   console.log(error);
+   alert("plz check your password ")
+}
+
+}
