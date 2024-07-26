787aa725a85e2af3bd08b96a3eb31c43a288f8ce
shoaib soomro <shoaibkhanu39@gmail.com>
2024-07-26 11:41:14 +0500

    userupdate-profile

--- firebase- 1/firebase/script/fitrstore-db.js
+++ firebase- 1/firebase/script/fitrstore-db.js
@@ -1,7 +1,6 @@
 
-
 const userDetial = document.querySelector(".userdetails")
-const editsprofile = document.getElementById("editsprofile")
+
 function createusercollection(user){
  
     firebase.firestore().collection("users")
@@ -12,8 +11,7 @@ function createusercollection(user){
         email:user.email,
         phone:"",
         specialty:"",
-        portfolioUrl:"",
-        experience:""
+        portfolioUrl:""
     })
 }
 
@@ -56,38 +54,17 @@ async function getuserinfoRealtime(userID){
                  <h3>${userinfo.name}</h3>
                  <h3>${userinfo.email}</h3>
                  <h3>${userinfo.phone}</h3>
-                  <h3>${userinfo.specialty}</h3>
-                  <h3>${userinfo.portfolioUrl}</h3>
-                <h3>${userinfo.experience}</h3> `
-                 editsprofile["name"].value = userinfo.name
-                 editsprofile["profileEmail"].value = userinfo.email
-                  editsprofile["phone"].value = userinfo.phone
-                  editsprofile["specialty"].value = userinfo.specialty
-                  editsprofile["portfolioUrl"].value = userinfo.portfolioUrl
-                  editsprofile["experience"].value = userinfo.experience
+                 `
                 }
             }
         })
+    
+    
+      
     } else {
          userDetial.innerHTML = `
         <h3>please login</h3>
         `
-    } 
-}
-function userupdate(e){
- e.preventDefault()
- const userDocRef = firebase
- .firestore()
- .collection("users")
- .doc(firebase.auth().currentUser.uid);
-
-userDocRef.update({
-    name: editsprofile["name"].value,
-    email : editsprofile["profileEmail"].value,
-    phone: editsprofile["phone"].value,
-    specialty : editsprofile["specialty"].value,
-    portfolioUrl: editsprofile["portfolioUrl"].value,
-    experience: editsprofile["experience"].value
-})
-
+    }
+ 
 }
