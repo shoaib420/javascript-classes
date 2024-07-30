e06b8a44c22c05cb1726433bcfbb8c8885b1adcd
shoaib soomro <shoaibkhanu39@gmail.com>
2024-07-30 11:45:35 +0500

    firebase-img-upload

--- firebase/script/fitrstore-db.js
+++ firebase/script/fitrstore-db.js
@@ -1,5 +1,7 @@
 
 
+
+
 const userDetial = document.querySelector(".userdetails")
 const editsprofile = document.getElementById("editsprofile")
 function createusercollection(user){
@@ -53,18 +55,23 @@ async function getuserinfoRealtime(userID){
                 const userinfo = doc.data();
                 if(userinfo){
                  userDetial.innerHTML = `
-                 <h3>${userinfo.name}</h3>
-                 <h3>${userinfo.email}</h3>
-                 <h3>${userinfo.phone}</h3>
+                 <h3 style="color:greenyellow ; text-align: center; ">${userinfo.name}</h3>
+                 <h3 style="color:greenyellow ; text-align: center; ">${userinfo.email}</h3>
+                 <h3 class="hello">${userinfo.phone}</h3>
                   <h3>${userinfo.specialty}</h3>
                   <h3>${userinfo.portfolioUrl}</h3>
-                <h3>${userinfo.experience}</h3> `
+                <h3>${userinfo.experience}</h3> 
+                `
                  editsprofile["name"].value = userinfo.name
                  editsprofile["profileEmail"].value = userinfo.email
                   editsprofile["phone"].value = userinfo.phone
                   editsprofile["specialty"].value = userinfo.specialty
                   editsprofile["portfolioUrl"].value = userinfo.portfolioUrl
                   editsprofile["experience"].value = userinfo.experience
+                  
+                  if(firebase.auth().currentUser.photoURL){
+                      document.querySelector("#propic").src = firebase.auth().currentUser.photoURL 
+                  }
                 }
             }
         })
@@ -88,6 +95,37 @@ userDocRef.update({
     specialty : editsprofile["specialty"].value,
     portfolioUrl: editsprofile["portfolioUrl"].value,
     experience: editsprofile["experience"].value
+
+    
 })
 
+}
+
+function uploadimg (e){
+    // console.log(e.target.files);
+    const uid = firebase.auth().currentUser.uid;
+    const fileRef = firebase.storage().ref().child(`/users/${uid}/profile`);
+    const uploadTask = fileRef.put(e.target.files[0])
+
+    uploadTask.on('state_changed', 
+        (snapshot) => {
+         
+          var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          if(progress=='100')alert("uploaded pic")
+          
+        }, 
+        (error) => {
+          console.log(error);
+        }, 
+        () => {
+        
+          uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+            console.log('File available at', downloadURL);
+            firebase.auth().currentUser.updateProfile({
+                photoURL: downloadURL
+              })
+          });
+        }
+      );
 }
