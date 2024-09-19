564e2331a9fe6f9686c3954e06578aa8df8e2b40
shoaib soomro <shoaibkhanu39@gmail.com>
2024-09-19 10:37:19 +0500

    Update Login.jsx

--- React/react-firebase/src/component/Register/Login.jsx
+++ React/react-firebase/src/component/Register/Login.jsx
@@ -1,6 +1,20 @@
 import './Login.css';
+import React, { useState } from 'react'
+import {auth} from '../../firebase/FirebaseConfig'
+import { signInWithEmailAndPassword  } from "firebase/auth"
 
 function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword ] = useState("")
+
+   const Login = async()=>{
+    try {
+     const user = await signInWithEmailAndPassword(auth, email, password)
+     alert("Login successfull")
+    } catch (error) {
+     console.log(error);
+    }
+   }
   return (
     <div className="main">
      <div className="sub-main">
@@ -17,14 +31,20 @@ function Login() {
            <h1>Login Page</h1>
            <div>
            
-             <input type="text" placeholder="user name" className="name"/>
+             <input 
+             value={email}
+             onChange={(e)=> setEmail(e.target.value)}
+             type="text" placeholder="user name" className="name"/>
            </div>
            <div className="second-input">
             
-             <input type="password" placeholder="user name" className="name"/>
+             <input type="password" 
+             value={password}
+             onChange={(e)=> setPassword(e.target.value)}
+              placeholder="user name" className="name"/>
            </div>
           <div className="login-button">
-          <button>Login</button>
+          <button onClick={Login}>Login</button>
           </div>
            
             <p className="link">
